44085dc9267d02a7050f76513aa66eed848f2983
Morten Andersen <morten@hjemmesider.dk>
2022-09-08 11:13:18 +0200

    Update img.php
    zoom

--- files/img.php
+++ files/img.php
@@ -7,32 +7,24 @@ function simpleEvent_img() {
 
 		if(!empty($content) && empty($link)) {
 			echo '<a href="' . get_the_permalink() . '">';
-			echo '<div class="event-img">';
 				the_post_thumbnail('event-img');
-			echo '</div>';
 			echo '</a>';
 		} else if(!empty($link)) {
 			echo '<a href="' . esc_url( $link ) . '" target="_blank">';
-			echo '<div class="event-img">';
 				the_post_thumbnail('event-img');
-			echo '</div>';
 			echo '</a>';
 		}
 		else {
-			echo '<div class="event-img">';
 				the_post_thumbnail('event-img');
-			echo '</div>';
 		}
 }
 
 function event_img() {
 if ( has_post_thumbnail() ) {
-    echo '<div class="img-zoom">';
-    echo '<div class="overflow">';
+    echo '<div class="img-zoom event-img">';
       simpleEvent_img();
       echo '<div class="event-day-month">';
         simpleEvent_day_month();
-      echo '</div>';
     echo '</div>';
     echo '</div>';
 }
